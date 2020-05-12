1b3d8bacd3115faafa9a24e19f3d13ed3059c045
阿部祥三郎 <shozaburoabe@Shozaburos-MacBook-Pro.local>
2020-05-12 22:27:46 +0900

    be able to render tempreture and input city name

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+})
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Yokohama&appid=95950fddffd37fdebaaabfa1aca1e10a&units=metric";
-
+app.post("/", (req, res) => {
+  const query = req.body.cityName;
+  const apiKey = "95950fddffd37fdebaaabfa1aca1e10a";
+  const units = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${units}`;
+  
   https.get(url, response => {
     console.log(response.statusCode);
-
+  
     response.on("data", (data) => {
       const weatherData = JSON.parse(data);
       const main = weatherData.main;
@@ -17,13 +25,15 @@ app.get("/", (req, res) => {
       const weatherDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
       const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+  
       res.write(`<p>The weather is currently ${weatherDescription}</p>`);
-      res.write(`<h1>The temperature in Yokohama is ${temp} degree Celcius.</h1>`);
+      res.write(`<h1>The temperature in ${query} is ${temp} degree Celcius.</h1>`);
       res.write(`<img src="${imageUrl}">`);
+      res.write(`<div>${main}</div>`);
       res.send();
     })
   })
-
+ 
 })
 
 
